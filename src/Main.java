95a527b2173783d178f7bb0bbeb45d7d964c8a73
ParkSangsin <139088684+ParkSangsin@users.noreply.github.com>
2024-03-26 22:21:59 +0900

    3주차

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String myname = "ari";
+        int money = 0;
+        float point = 0;
+        boolean state = false;
+        char chr = 'a';
+
+        myname =
+    }
+}
